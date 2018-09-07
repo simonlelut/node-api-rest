490d5bb61e456d28fb856260743e742fb3a47685
simonlelut <simon.lelut@gmail.com>
2018-09-07 15:00:22 +0200

    change setting

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,15 +14,20 @@ module.exports = {
   // more http://pm2.keymetrics.io/docs/usage/deployment/
   deploy : {
     production : {
-      user : 'node',
+      
+      key: "/root/.ssh/newKey_private.perm",
+
+      user : 'simon',
       // Multi host is possible, just by passing IPs/hostname as an array
-      host : ["212.83.163.1", "212.83.163.2", "212.83.163.3"],
+      host : ["192.168.1.68"],
+
+      ssh_options: "StrictHostKeyChecking=no",
       // Branch
       ref  : 'origin/master',
       // Git repository to clone
       repo : 'git@github.com:simonlelut/node-api-rest.git',
       // Path of the application on target servers
-      path : '/',
+      path : '/var/www/myapp',
       // Commands to be executed on the server after the repo has been cloned
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js'
     }
