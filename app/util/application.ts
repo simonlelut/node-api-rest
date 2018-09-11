75f3a8b066ecd66a04be163df3da993557e06883
simonlelut <simon.lelut@gmail.com>
2018-09-11 12:16:29 +0200

    setup test to user crud

---
+++ app/util/application.ts
@@ -0,0 +1,90 @@
+import * as debug from 'debug';
+import * as http from 'http';
+import * as express from 'express';
+import { Express, Request, Response, NextFunction } from "express";
+import router from '../routes/routes';
+import * as bodyParser from 'body-parser';
+import * as compression from 'compression';
+import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
+import * as helmet from 'helmet';
+import "reflect-metadata";
+import * as morgan from 'morgan';
+import {createConnection, Connection} from "typeorm";
+//get config
+const config = require("../../config/config.json");
+let dataseConfig = config.databaseConfig;
+
+//for typescript
+debug('ts-express:server');
+
+export class Application {
+
+    public static getApp(): Promise<Express | void> {
+        //variablesx
+        const port:number = 3000;
+        const app: express.Express = express();
+
+        //global variables
+        app.set('port', port);
+        app.set("config",config)
+
+        // middleware
+        app.use(bodyParser.urlencoded({ extended: false }));
+        app.use(bodyParser.json());
+        app.use(cookieParser());
+        app.use(compression());
+        app.use(helmet());
+        app.use(cors());
+
+        if(process.env.NODE_ENV === "test")
+            dataseConfig = config.databaseTest;
+        else
+            app.use(morgan('combined'));
+
+
+        // cors
+        app.use((req, res, next) => {
+            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS ');
+            res.header(
+                'Access-Control-Allow-Headers',
+                'Origin, X-Requested-With,' +
+                ' Content-Type, Accept,' +
+                ' Authorization,' +
+                ' Access-Control-Allow-Credentials'
+            );
+            res.header('Access-Control-Allow-Credentials', 'true');
+            next();
+        });
+
+        //Routes
+        app.use(router);
+
+        const server: http.Server = http.createServer(app);
+
+        //connection database
+        return createConnection(dataseConfig).then(async () => {
+
+            // server listen
+            await server.listen(port);
+            console.log(`Server running on port: ${port}`)
+
+            // here you can start to work with your entities
+            console.info("database connection set");
+            return app;
+            
+        }).catch(error => console.info(error));
+
+
+        // server handlers
+        server.on(
+            'error',
+            (error) => console.error(error));
+    }
+}
+
+
+    
+    
+    
+    
---
+++ tests/user.ts
@@ -0,0 +1,51 @@
+process.env.NODE_ENV = 'test';
+
+import {User} from "../app/entity/User";
+import {getConnection} from "typeorm";
+import { Application } from "../app/util/application";
+
+//Require the dev-dependencies
+let chai = require('chai')
+  , chaiHttp = require('chai-http');
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+
+//Our parent block
+describe('User', () => {
+    let server;
+    let connection;
+
+    beforeEach(done => { 
+
+        connection.getRepository(User).delete({});
+        done();
+    }); 
+
+    before(done => { 
+        Application.getApp().then(async a => { 
+            server = a;
+            connection = await getConnection();
+            done();
+        }).catch(done);
+    });
+
+    /*
+    * Test the /GET route
+    */
+    describe('/GET users', () => {
+    it('it should GET all the users', (done) => {
+        console.log()
+        chai.request(server)
+            .get('/users')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(0);
+            done();
+            });
+        });
+    });
+});
