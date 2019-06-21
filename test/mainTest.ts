bd55c160abbd80640559810ccacfd5e2c198ea95
simonlelut <simon.lelut@gmail.com>
2019-06-21 12:47:05 +0200

    test

--- test/mainTest.ts
+++ test/mainTest.ts
@@ -4,24 +4,24 @@ import { user } from './User/userTest'
 
 import {getConnection} from "typeorm";
 import { Application } from "../app/server";
-let config = require("../config/config.dist.json");
 
 export let server;
 export let connection;
 
 before(async () => { 
-    await Application.getApp(config).then(async serv => { 
+    /*
+    await Application.getApp().then(async serv => { 
        
         server = serv;
         connection = await getConnection();
     });
+    */
 });
 
 
 after(async () => {
-    Application.stop();
+    //Application.stop();
 });
 
 //Our parent block
-describe('User get test', user );
-
+//describe('User get test', user );
